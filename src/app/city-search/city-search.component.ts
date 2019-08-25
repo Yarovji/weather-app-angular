b7b4063b391e4a57aa203daa8da2a71ba7c2929e
Yaroslav Virstiuk <yarovji@gmail.com>
2019-08-25 17:08:14 +0300

    add region to search

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WeatherSService } from '../weather-s.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-city-search',
   templateUrl: './city-search.component.html',
   styleUrls: ['./city-search.component.scss']
 })
-export class CitySearchComponent implements OnInit {
+export class CitySearchComponent implements OnInit, OnDestroy {
   resSetFromServis: Set<{}>;
+  ollCountries: Array<string>;
+  chooseCountryName = "Ukraine";
+  unsubscribe: Subscription;
+
 
   constructor(private weatherSService: WeatherSService) { }
 
   ngOnInit() {
-
+    this.weatherSService.allCountry();
+    this.unsubscribe =  this.weatherSService.dataCountry.subscribe((res: Array<string>) => {
+      this.ollCountries = [...res];
+    });
   }
 
   searchCity(cityName: string): void {
-    this.weatherSService.findCity(cityName);
-    this.resSetFromServis = this.weatherSService.resToShow2;
+    if (cityName.length >= 1) {
+      this.weatherSService.findCity(cityName, this.chooseCountryName);
+      this.resSetFromServis = this.weatherSService.resToShow2;
+    }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.unsubscribe();
   }
 
 }
--- src/app/weather-s.service.ts
+++ src/app/weather-s.service.ts
@@ -1,93 +1,75 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/internal/operators/map';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherSService {
-  myData: any;
+  arrayNamesOfOllCountries = [];
+  arrOfOllCountriesName: any;
   myData2 = [];
-  public resToShow = [];
   public resToShow2 = new Set();
+  dataCountry = new Subject();
 
   constructor(private httpService: HttpClient) {}
 
-  findCity(cityName: string) {
+  allCountry() {
     this.httpService.get('https://raw.githubusercontent.com/solominh/country_state_city_data/master/data/countries.json')
-      .pipe(map(res => {
-          let arr = [];
+      .pipe(
+        map(arrayOfOllCountries => {
+          const arr = [];
           // tslint:disable-next-line:forin
-          for (const key in res) {
-            arr.push(res[key].country);
+          for (const key in arrayOfOllCountries) {
+            arr.push(arrayOfOllCountries[key].country);
           }
-          console.log(arr);
           return arr;
-        })
-      )
-      .subscribe(data => {
-        this.resToShow.length = 0;
+        })).subscribe(data => {
+        this.arrOfOllCountriesName = data;
+        this.dataCountry.next(this.arrOfOllCountriesName);
+      });
+  }
+
+  findCity(cityName: string, countryName: string) {
+    this.myData2.length = 0;
+    this.httpService.get(`https://raw.githubusercontent.com/solominh/country_state_city_data/master/data/region/${countryName}.json`)
+      .pipe( map(res => res.regions)).subscribe(data2 => {
         this.resToShow2.clear();
-        this.myData = data;
-        this.httpService
-          .get(
-            `https://raw.githubusercontent.com/solominh/country_state_city_data/master/data/region/${this.myData[230]}.json`
-          )
-          .pipe(
-            map(res => {
-              return res.regions;
-            })
-          )
-          .subscribe(data2 => {
-            // tslint:disable-next-line:forin
-            for (const key in data2) {
-              this.myData2.push(data2[key].toponymName);
-            }
-            // tslint:disable-next-line:prefer-for-of
-            for (let citiLop = 0; citiLop < this.myData2.length; citiLop++) {
-              this.httpService
-                .get(
-                  // tslint:disable-next-line:max-line-length
-                  `https://raw.githubusercontent.com/solominh/country_state_city_data/master/data/region_city_data/${this.myData[230]}/${this.myData2[citiLop]}.json`
-                )
-                .pipe(
-                  map(res => {
-                    return res.cities;
-                  })
-                )
-                .subscribe(res3 => {
-                  let arr3_alt = [];
-                  let arr3_true = [];
-                  // tslint:disable-next-line:forin
-                  for (const key in res3) {
-                    arr3_alt.push(res3[key].alternatenames);
-                    arr3_true.push(res3[key].asciiname);
+        // tslint:disable-next-line:forin
+        for (const key in data2) {
+          this.myData2.push(data2[key].toponymName);
+        }
+        let arrOfOblastName = []
+        // tslint:disable-next-line:prefer-for-of
+        for (let citiLop = 0; citiLop < this.myData2.length; citiLop++) {
+          this.httpService.get(
+              // tslint:disable-next-line:max-line-length
+              `https://raw.githubusercontent.com/solominh/country_state_city_data/master/data/region_city_data/${countryName}/${this.myData2[citiLop]}.json`
+            ).subscribe((res3: {region_data: object, country_data: object, cities: any}) => {
+                arrOfOblastName[res3.region_data.adminCode1] = res3.region_data.toponymName;
+                let arr3_alt = [];
+                let arr3_true = [];
+                const arrOfRegion = [];
+              // tslint:disable-next-line:forin
+                for (const key in res3.cities) {
+                arr3_alt.push(res3.cities[key].alternatenames);
+                arr3_true.push(res3.cities[key].asciiname);
+                arrOfRegion.push(res3.cities[key].admin1_code);
+              }
+                for (let i = 0; i < arr3_alt.length; i++) {
+                // tslint:disable-next-line:prefer-for-of
+                for (let alterKilkist = 0; alterKilkist < arr3_alt[i].length; alterKilkist++) {
+                  const newarr = arr3_alt[i][alterKilkist].split('');
+                  newarr.length = cityName.length;
+                  if (newarr.join('').toLowerCase() === cityName.toLowerCase()) {
+                    this.resToShow2.add(`${arr3_true[i]}, ${arrOfOblastName[arrOfRegion[i]]}`);
                   }
-                  for (let i = 0; i < arr3_alt.length; i++) {
-                    // tslint:disable-next-line:prefer-for-of
-                    for (
-                      let alterKilkist = 0;
-                      alterKilkist < arr3_alt[i].length;
-                      alterKilkist++
-                    ) {
-                      let newarr = arr3_alt[i][alterKilkist].split('');
-                      newarr.length = cityName.length;
-                      // console.log(newarr.join('').toLowerCase());
-                      // console.log(cityName.toLowerCase());
-                      if (
-                        newarr.join('').toLowerCase() == cityName.toLowerCase()
-                      ) {
-
-                        this.resToShow.push(arr3_true[i]);
-                        this.resToShow2.add(arr3_true[i]);
-                        // console.log(arr3_true[i]);
-                      }
-                    }
-
-                  }
-                });
-            }
-          });
+                }
+              }
+            });
+        }
       });
   }
 }
+
